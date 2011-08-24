accfbaf7a6beca86e0a95bff6816fdfd5bb49c87
Paul Van de Vreede <paul@vdvreede.net>
2011-08-24 23:24:36 +1000

    Reworked widget into widget class.

--- pv_login_widget.php
+++ pv_login_widget.php
@@ -8,28 +8,46 @@ Version: 1
 Author URI: http://www.vdvreede.net
 */
 
-add_action("plugins_loaded", "pvl_register_widget");
+add_action( 'widgets_init', create_function( '', 'return register_widget("PV_Login_Widget");' ) );
 
-function pvl_register_widget() {
+class PV_Login_Widget extends WP_Widget {
+	/** constructor */
+	function FooWidget() {
+		parent::WP_Widget( 'pv_login_widget', $name = 'PV Login Widget' );
+	}
 
-	register_sidebar_widget('PV Login Widget', 'pvl_render_widget');
-	
-}
+	/** @see WP_Widget::widget */
+	function widget( $args, $instance ) {
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		echo $before_widget;
+		if ( $title )
+			echo $before_title . $title . $after_title; ?>
+		<p>Hello there</p>
+		<?php echo $after_widget;
+	}
 
-function pvl_render_widget() {
-?>
+	/** @see WP_Widget::update */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		return $instance;
+	}
 
-	<form method="post" action="http://localhost/wp-login.php">
-		
-		<input type="text" name="log" />
-		
-		<input type="password" name="pwd" />
-		
-		<input type="submit" value="Log in" />
-		
-		<input type="hidden" name="redirect_to" value="http://localhost" />
-	
-	</form>
-	
-<?php
+	/** @see WP_Widget::form */
+	function form( $instance ) {
+		if ( $instance ) {
+			$title = esc_attr( $instance[ 'title' ] );
+		}
+		else {
+			$title = __( 'New title', 'text_domain' );
+		}
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<?php 
+	}
+
 }
